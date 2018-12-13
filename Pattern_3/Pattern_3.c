d701e1e37b0635b7ea10ecf813d2d7d28b6f48ba
satyajit.daulaguphu <satyajit.daulaguphu@RINPC602>
2018-12-13 21:48:54 +0530

    Add project files.

---
+++ Pattern_3/Pattern_3.c
@@ -0,0 +1,26 @@
+/*
+Program to print the pattern below:-
+
+12345
+12345
+12345
+12345
+12345
+
+*/
+
+#include <stdio.h>
+
+void main()
+{
+	int i, j;
+
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 1; j <= 5; j++)
+		{
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+}
